c6e839a8af68f723fceb4b3032aed2f2c1e2f2ec
bpqq <bpqvgq@gmail.com>
2021-06-11 17:50:20 +0300

    Some fixes

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,11 @@
 FROM archlinux:latest AS build
 
 RUN pacman -Sy --noconfirm && pacman -S --noconfirm python python-pip python-virtualenv git
-CMD ["echo", "Python, pip and virtualenv has been successfully installed."]
 
 WORKDIR /app/
 
 RUN git clone https://github.com/bpqq/proxy-pool.git
-RUN python3 -m venv venv
-RUN . venv/bin/activate
+WORKDIR /app/proxy-pool/
 RUN pip install --upgrade pip
 RUN pip install -r /app/proxy-pool/requirements.txt
+
--- sort_proxies.py
+++ sort_proxies.py
@@ -3,7 +3,6 @@ with open('proxies.txt', 'r') as f:
     proxies = list(map(lambda x: x.rstrip('\n').split(' '), f.readlines()))
     proxies_edit = list(map(lambda x: {'ip:port': x[3], 'type': x[2].strip('[]'), 'ping': x[1].rstrip('s')}, proxies))
     proxies_sorted = sorted(proxies_edit, key=lambda k: k['ping'], reverse=False)
-    print(proxies_sorted)
     with open('proxies_sorted.txt', 'w') as f2:
         for proxy in proxies_sorted:
             f2.write('{} {}\n'.format(proxy['ip:port'], proxy['type'].replace(':', '').replace('Anonymous', '').replace('Transparent', '').replace('High', '')))
