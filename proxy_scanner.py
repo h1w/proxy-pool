af72bc58d4568d29991953b9271910b40a4494c4
bpqq <bpqvgq@gmail.com>
2021-06-11 15:41:13 +0300

    Proxy list from sites

---
+++ proxy_scanner.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+import asyncio
+from proxybroker import Broker
+
+async def save(proxies, filename):
+    with open(filename, 'w') as f: # Make file empty
+        f.write('')
+        f.close()
+    with open(filename, 'a') as f: # Add proxy with description to file
+        while True:
+            proxy = await proxies.get()
+            if proxy is None: # Exit when proxies run out
+                break
+            proxy = str(proxy).strip('<>').lstrip('Proxy ').split(' ')
+            f.write("{} {} {} {}\n".format(proxy[0], proxy[1], "".join(i for i in proxy[2:-1]), proxy[-1]))
+            f.flush()
+    f.close()
+
+proxies = asyncio.Queue()
+broker = Broker(proxies)
+tasks = asyncio.gather(
+    broker.find(types=['HTTP', 'HTTPS', 'SOCKS4', 'SOCKS5'], lvl="High"),
+    save(proxies, filename='proxies.txt'),
+)
+loop = asyncio.get_event_loop()
+loop.run_until_complete(tasks)
